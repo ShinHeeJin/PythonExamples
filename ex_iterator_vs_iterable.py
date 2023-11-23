e33e5d414866033ee48f8dedac277558a291dce7
ShinHeeJin <sisesu92@gmail.com>
2023-11-23 21:23:20 +0900

    add : Custom Iterable & Iterator

--- ex_iterator_vs_iterable.py
+++ ex_iterator_vs_iterable.py
@@ -1,3 +1,7 @@
+import random
+from dataclasses import dataclass, field
+from typing import Iterable, Iterator, Sequence
+
 iterable = [1, 2, 3]
 iterator = iterable.__iter__()
 assert next(iterator) == 1
@@ -11,15 +15,10 @@ assert next(iterator) == "b"
 assert next(iterator) == "c"
 
 
-import random
-
-__function = lambda: random.randint(0, 10)
 __sentinel = 2
-iterator = iter(__function, __sentinel)
+iterator = iter(lambda: random.randint(0, 10), __sentinel)
 assert __sentinel not in [i for i in iterator]
 
-
-from typing import Iterable, Iterator, Sequence
 
 assert isinstance("123", Iterable)
 assert isinstance([1, 2, 3], Iterable)
@@ -34,3 +33,41 @@ assert isinstance((1, 2, 3), Sequence)
 assert not isinstance({"1": 1, "2": 2, "3": 3}, Sequence)
 
 assert isinstance([1, 2, 3].__iter__(), Iterator)
+
+# --------------------------------------------------------------------
+
+
+@dataclass
+class MyIterable:
+    start: int
+    end: int
+
+    def __iter__(self):
+        return MyIterator(self.start, self.end)
+
+
+@dataclass
+class MyIterator:
+    start: int
+    end: int
+    current: int = field(init=False)
+
+    def __post_init__(self):
+        self.current = self.start
+
+    def __next__(self):
+        if self.current > self.end:
+            raise StopIteration
+
+        self.current += 1
+        return self.current - 1
+
+
+assert [each for each in MyIterable(1, 5)] == [1, 2, 3, 4, 5]
+
+iterator = MyIterable(1, 5).__iter__()
+assert next(iterator) == 1
+assert next(iterator) == 2
+assert next(iterator) == 3
+assert next(iterator) == 4
+assert next(iterator) == 5
