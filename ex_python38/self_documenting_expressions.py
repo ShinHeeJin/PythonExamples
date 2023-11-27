decab7a4c0b9be87fb666983ace93546c732f0b2
ShinHeeJin <sisesu92@gmail.com>
2023-11-27 18:19:22 +0900

    self_documenting_expressions

---
+++ ex_python38/self_documenting_expressions.py
@@ -0,0 +1,11 @@
+# f-string에서 평가식(self-documenting-expressions)을 위한 =기호 추가
+
+name = "test"
+print(f"name = {name}")  # name = test
+print(f"{name = }")  # name = 'test'
+
+pi = 3.141592
+print(f"{pi = :.3f}")  # pi = 3.142
+
+number = 30000
+print(f"{number = :,d}")  # pi = 3.142
