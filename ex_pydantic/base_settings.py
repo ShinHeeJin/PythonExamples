a23ffba18af902a07c6490fb7c61210f55d1a20d
ShinHeeJin <sisesu92@gmail.com>
2023-12-22 01:55:07 +0900

    pydantic validator decorator

--- ex_pydantic/base_settings.py
+++ ex_pydantic/base_settings.py
@@ -1,4 +1,8 @@
-from pydantic import Field
+import ipaddress
+import socket
+
+import pytest
+from pydantic import Field, validator
 from pydantic_settings import BaseSettings
 
 
@@ -9,6 +13,33 @@ class DBConfig(BaseSettings):
     class Config:
         env_file = ".env"
 
+    @validator("host", pre=True)  # default validator보다 먼저 실행
+    def check_host(cls, host):
+        try:
+            ipaddress.ip_address(host)
+            return host
+        except ValueError:
+            try:
+                socket.gethostbyname(host)
+                return host
+            except socket.error:
+                return ValueError("Invalid host")
+
+    @validator("port")
+    def check_port(cls, port):
+        try:
+            port = int(port)
+        except ValueError:
+            raise ValueError("Invalid port")
+
+        if 1 <= port <= 65535:
+            return port
+        raise ValueError("Invalid port")
+
 
 def test_base_settings():
-    assert DBConfig().model_dump() == {"host": "123.123.123.123", "port": 3306123}
+    assert DBConfig().model_dump() == {"host": "123.123.123.123", "port": 65534}
+    assert DBConfig(host="example.com", port="80").model_dump() == {"host": "example.com", "port": 80}
+
+    with pytest.raises(ValueError):
+        DBConfig(host="---", port="80")
