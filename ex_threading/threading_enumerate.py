099e67303e085a9aadc918525799f40ce1a90246
ShinHeeJin <sisesu92@gmail.com>
2023-12-24 23:53:40 +0900

    threading_enumerate

---
+++ ex_threading/threading_enumerate.py
@@ -0,0 +1,15 @@
+import random
+import threading
+import time
+
+
+def executedThread(i):
+    print(f"Thread {i} started")
+    time.sleep(random.randint(1, 10))
+    print(f"Thread {i} finished")
+
+
+for i in range(10):
+    thread = threading.Thread(target=executedThread, args=(i,))
+    thread.start()
+    print(f"Active Threads : {threading.enumerate()}")
