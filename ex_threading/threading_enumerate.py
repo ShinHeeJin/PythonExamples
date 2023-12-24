463c8eef7ee67092caa7fb3f02c22d869566a19f
ShinHeeJin <sisesu92@gmail.com>
2023-12-25 00:05:58 +0900

    Current Thread

--- ex_threading/threading_enumerate.py
+++ ex_threading/threading_enumerate.py
@@ -4,6 +4,9 @@ import time
 
 
 def executedThread(i):
+    print(f"Current Thread : {threading.current_thread()}")
+    # # Current Thread : <Thread(Thread-10 (executedThread), started 123145542021120)>
+
     print(f"Thread {i} started")
     time.sleep(random.randint(1, 10))
     print(f"Thread {i} finished")
@@ -13,4 +16,6 @@ for i in range(10):
     thread = threading.Thread(target=executedThread, args=(i,))
     thread.start()
     print(f"Active Threads : {threading.enumerate()}")
-    print(f"Active Thread Count : {threading.active_count()}")
+    # [<_MainThread(MainThread, started 140704300631808)>, <Thread(Thread-1 (executedThread), started 123145390915584)>]
+
+    print(f"Active Thread Count : {threading.active_count()}")  # 2
