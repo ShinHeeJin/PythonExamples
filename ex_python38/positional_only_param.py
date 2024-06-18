02891cd52b037a4935f9e8e1a7d072bed56289ae
ShinHeeJin <sisesu92@gmail.com>
2024-06-18 10:16:51 +0900

    ✅ position & keword only Inheritance Test 케이스 추가

--- ex_python38/positional_only_param.py
+++ ex_python38/positional_only_param.py
@@ -45,3 +45,28 @@ def test_positional_only_param3():
     assert func(10, 30, c=40, d=50, e=60, f=70) == (10, 30, 40, 50, 60, 70)
     with pytest.raises(TypeError, match="got some positional-only arguments passed as keyword arguments"):
         func(a=10, b=30, c=40, d=50, e=60, f=70)
+
+
+def test_inheritance():
+
+    class Parent:
+        def test_func(self, a: str = None, /, b: int = None, *, c: float = 0.0):
+            print(f"{a=} {b=} {c=}")
+
+    class Child(Parent):
+
+        def test_func(self, *args, **kargs):
+            super().test_func(*args, **kargs)
+
+    child = Child()
+    child.test_func(1)
+    child.test_func(b=1)
+    child.test_func(c=1)
+    child.test_func(1, b=2, c=3.0)
+    child.test_func(1, 2, c=3.0)
+
+    with pytest.raises(TypeError):
+        child.test_func(1, 2, 3)
+
+    with pytest.raises(TypeError):
+        child.test_func(a=1)
